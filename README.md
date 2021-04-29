1fc1527d2271695680e0c1c06ce59377d2ef4454
jinzeng <jinzeng@ebay.com>
2021-04-29 11:15:37 +0800

    Find a good article about javascript

--- README.md
+++ README.md
@@ -2,5 +2,21 @@
 
 Document my experience in learning Typescript
 
-
 # [二分法](leetcode-js/median-of-two-sorted-arrays.js)
+
+# A great article about javascript questions
+
+## From basic to advanced  
+[查看原文](https://github.com/lydiahallie/javascript-questions)
+
+1. What is the output?
+```javascript
+function sayHi() {
+  console.log(name);
+  console.log(age);
+  var name = 'Lydia';
+  let age = 21;
+}
+
+sayHi();
+```
--- leetcode-ts/two-sum.ts
+++ leetcode-ts/two-sum.ts
@@ -4,25 +4,25 @@
  * @return {number[]}
  * https://leetcode-cn.com/problems/two-sum/submissions/
  */
-nums = [3,3];
+const nums = [3,3];
 const target = 6;
 
 function tSum(nums: number[], target: number): number[] {
     const result: number[] = []
-  
+
     for (let i = 0; i <= nums.length; i++) {
       let avaliableIndex = null
       const avaliableValue = nums.some((num, index) => {
         avaliableIndex = index
         return index !== i && nums[i] + num === target
       })
-  
+
       if (avaliableValue) {
         result.push(i, avaliableIndex)
         break
       }
     }
-  
+
     return result
   }
 
